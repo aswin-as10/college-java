3c0d46670a926e215113f6e1b67fe1ec21009ddc
aswin-as10 <aswinas047@gmail.com>
2024-11-16 15:20:20 +0530

    Create file handling.java

---
+++ file handling.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class file {
+    public static void main(String[]args)throws IOException{
+        try{
+         FileInputStream fin=new FileInputStream("C:\\Users\\aswin\\OneDrive\\Desktop\\c\\d.txt");
+         FileOutputStream fout=new FileOutputStream("C:\\Users\\aswin\\OneDrive\\Desktop\\c\\g.txt");
+         int i=0;
+         while((i=fin.read())!=-1){
+            fout.write(i);
+             }
+             System.out.println("message written to file");
+        }
+        catch(IOException e){
+                    System.err.println("an error occured");
+        }
+    }
+}
